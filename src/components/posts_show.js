4cf5138fccb07999ec2736d5b04f5653431a2637
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2018-11-05 21:55:59 -0200

    Fix post request

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
     const { props } = this;
     const { id } = props.match.params;
-    props.fetchPosts(id);
+    props.fetchPost(id);
   }
 
   render() {
@@ -19,10 +19,7 @@ class PostsShow extends Component {
     return (
       <div>
         <h3>{post.title}</h3>    
-        <h6>
-          Categories: 
-          {post.categories}
-        </h6>
+        <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
       </div>
     );
@@ -33,4 +30,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPosts })(PostsShow);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
