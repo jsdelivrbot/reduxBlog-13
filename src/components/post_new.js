c65dedb66a950fe4098bb51f778d83989a40da50
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2018-11-04 23:11:55 -0200

    Implement new post submit component

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   
   onSubmit(values) {
     // this === component
-
-    console.log(values);
+    const { props } = this;
+    props.createPost(values, () => {
+      props.history.push('/');
+    });
   }
   
   renderField(field) {
@@ -52,6 +57,7 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
@@ -77,4 +83,6 @@ function validate(values) {
 export default reduxForm({
   validate: validate,
   form: 'PostNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+);
